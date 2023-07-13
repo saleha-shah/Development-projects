e51a2c89b9030fee536874f5ba4c58478905ee09
Saleha Shahzad <saleha.shahzad@A006-01264.local>
2023-07-13 12:30:45 +0500

    snake_case used

--- weather_reading.py
+++ weather_reading.py
@@ -2,11 +2,11 @@ from datetime import datetime
 
 
 class WeatherReading:
-    def __init__(self, date, MaxTemp, MinTemp, MaxHumidity, MeanHumidity):
+    def __init__(self, date, max_temp, min_temp, max_humidity, mean_humidity):
         self.date = datetime.strptime(date, '%Y-%m-%d')
-        self.maxtemp = MaxTemp
-        self.mintemp = MinTemp
-        self.maxhumid = MaxHumidity
-        self.meanhumidity = MeanHumidity
+        self.max_temp = max_temp
+        self.min_temp = min_temp
+        self.max_humid = max_humidity
+        self.mean_humidity = mean_humidity
 
 
