6c034c0050f1e8ea3dad2c8e3c9b51e350995696
Saleha Shahzad <saleha.shahzad@A006-01264.local>
2023-07-13 12:29:57 +0500

    multiline f string

--- weather_report.py
+++ weather_report.py
@@ -7,28 +7,30 @@ class WeatherReport:
 
     def generate_yearly_report(self):
         if self.results is None:
-            return "No data available."
-        report = f"Highest: {int(self.results['highest_temp'])}C "
-        report += f"on {self.results['highest_temp_date']}\n"
-        report += f"Lowest: {int(self.results['lowest_temp'])}C "
-        report += f"on {self.results['lowest_temp_date']}\n"
-        report += f"Humidity: {int(self.results['highest_humidity'])}%"
-        report += f" on {self.results['highest_humidity_date']}"
+            return 'No data available.'
+        report = (f"Yearly Report\n"
+                  f"Highest: {int(self.results['highest_temp'])}C"
+                  f" on {self.results['highest_temp_date']}\n"
+                  f"Lowest: {int(self.results['lowest_temp'])}C"
+                  f" on {self.results['lowest_temp_date']}\n"
+                  f"Humidity: {int(self.results['highest_humidity'])}% "
+                  f"on {self.results['highest_humidity_date']}")
         return report
 
-    def generate_monthly_report(self):
+    def generate_monthly_report(self, year, month):
         if self.results is None:
-            return "No data available."
-        report = f"Highest Average: {int(self.results['highest_avg'])}C\n"
-        report += f"Lowest Average: {int(self.results['lowest_avg'])}C\n"
-        report += "Average Mean Humidity:" 
-        report += f"{abs(int(self.results['humid_avg']))}%"
+            return 'No data available.'
+
+        report = (f"Monthly Report\n"
+                  f"Highest Average: {int(self.results['highest_avg'])}C\n"
+                  f"Lowest Average: {int(self.results['lowest_avg'])}C\n"
+                  f"Average Mean Humidity:"
+                  f"{abs(int(self.results['humid_avg']))}%")
         return report
 
     def generate_monthly_chart(self, year, month):
         if len(self.results) == 0:
-            return "No data available."
-        print(self.results)
+            return 'No data available.'
 
         month_name = datetime(year=year, month=month, day=1).strftime('%B %Y')
         chart = f"{month_name}\n"
@@ -37,12 +39,12 @@ class WeatherReport:
             highest_temp = extremes['highest_temp']
             lowest_temp = extremes['lowest_temp']
             if lowest_temp >= 0:
-                sign = "+"
+                sign = '+'
             else:
-                sign = "-"
+                sign = '-'
 
-            chart += f"{str(day).zfill(2)} "
-            chart += "\033[91m+\033[0m" * int(highest_temp)
-            chart += f"\033[94m{sign}\033[0m" * abs(int(lowest_temp))
-            chart += f" {int(highest_temp)}C - {int(lowest_temp)}C\n"
+            chart += f'{str(day).zfill(2)} '
+            chart += '\033[91m+\033[0m' * int(highest_temp)
+            chart += f'\033[94m{sign}\033[0m' * abs(int(lowest_temp))
+            chart += f' {int(highest_temp)}C - {int(lowest_temp)}C\n'
         return chart
