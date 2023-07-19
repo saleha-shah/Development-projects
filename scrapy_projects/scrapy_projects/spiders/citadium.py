5505e92da626aaac8a0b00903167a836ef74274b
Saleha Shahzad <saleha.shahzad@192.168.10.3>
2023-07-19 18:07:58 +0500

    separate attribute methods

--- scrapy_projects/scrapy_projects/spiders/citadium.py
+++ scrapy_projects/scrapy_projects/spiders/citadium.py
@@ -10,39 +10,86 @@ class CitadiumSpider(Spider):
     allowed_domains = ["www.citadium.com"]
     start_urls = ["https://www.citadium.com/fr/fr/femme"]
     rules = (
-        Rule(LinkExtractor(allow='page/', deny='tag/'), callback='parse_product_links', follow=True),
+        Rule(LinkExtractor(allow="page/", deny="tag/"), callback="parse_product_links", follow=True)
     )
 
     def parse(self, response):
         url = response.css("li.container-submenu a.link_style-1::attr(href)")[1].get()
-        yield response.follow(url, self.parse_brand_links)
-        yield from self.parse_product_links(response)
+        page_name = self.extract_page_name(response)
 
-    def parse_brand_links(self, response):
+        yield response.follow(url, self.parse_brand_links, cb_kwargs={"trail": [[page_name, url]]})
+        yield from self.parse_product_links(response, [[page_name, url]])
+
+    def parse_brand_links(self, response, trail):
+        page_name = self.extract_page_name(response)
         for brand_url in response.css("div.letter-header a::attr(href)").getall():
-            yield response.follow(brand_url, self.parse_product_links)
+            yield response.follow(brand_url, self.parse_product_links,
+                                  cb_kwargs={"trail": trail + [page_name, brand_url]})
 
-    def parse_product_links(self, response):
+    def parse_product_links(self, response, trail):
+        page_name = self.extract_page_name(response)
         for product_url in response.css("#view-all-items .position-relative::attr(href)").getall():
-            yield response.follow(product_url, self.parse_products)
+            yield response.follow(product_url, self.parse_products,
+                                  cb_kwargs={"trail": trail + [page_name, product_url]})
 
-    def parse_products(self, response):
+    def parse_products(self, response, trail):
         item = ScrapyProjectsItem()
-        item["brand"] = response.css("p.align-items-center::text").get().strip()
-        item["category"] = response.css('div.product-infos-text-seo span a::text').getall()
-        item["currency"] = response.css("script::text").re_first(r'"priceCurrency":"(\w+)"')
-        item["description"] = [text.strip().replace('-', '')
-                               for text in response.css("div.op-tab-0::text").getall() 
-                               if text.strip()]
-        item["gender"] = response.css("div#ariane li:nth-child(2) span::text").get()
-        item["image_urls"] = response.css(".w-100.lzy_img.d-none::attr(src)").getall()
-        item["lang"] = response.css("html::attr(lang)").get().split("-")[0]
-        item["market"] = response.css("html::attr(lang)").get().split("-")[1]
-        item["name"] = response.css("h1.fs-18.mb-3.d-flex.flex-column::text")[1].getall()[0].strip()
-        item["price"] = response.css("p.text_style-11 span::text").get().split("\xa0")[0].replace(",", ".")
-        item["retailer_sku"] = response.css("link[rel='canonical']::attr(href)").get().split("-")[-1]
-        item["trail"] = [[element.css('span::text').get().strip(), element.css('a::attr(href)').get()] 
-                         if element.css('a') else element.css('span::text').get().strip() 
-                         for element in response.css('.block-breadcrum [itemprop="itemListElement"]')]
-        item["url"] = response.css("link[rel='canonical']::attr(href)").get()
+        item["brand"] = self.extract_product_brand(response)
+        item["category"] = self.extract_product_category(response)
+        item["currency"] = self.extract_product_currency(response)
+        item["description"] = self.extract_product_description(response)
+        item["gender"] = self.extract_product_gender(response)
+        item["image_urls"] = self.extract_product_image_urls(response)
+        item["lang"] = self.extract_product_language(response)
+        item["market"] = self.extract_product_market(response)
+        item["name"] = self.extract_product_name(response)
+        item["price"] = self.extract_product_price(response)
+        item["retailer_sku"] = self.extract_product_retailer_sku(response)
+        item["trail"] = trail
+        item["url"] = self.extract_product_url(response)
+
         yield item
+
+    def extract_page_name(self, response):
+        page_names = response.css("div.block-breadcrum span[itemprop='name']::text").getall()
+        if page_names:
+            return page_names[-1].strip()
+        return ""
+
+    def extract_product_brand(self, response):
+        return response.css("p.align-items-center::text").get().strip()
+
+    def extract_product_category(self, response):
+        return response.css("div.product-infos-text-seo span a::text").getall()
+
+    def extract_product_currency(self, response):
+        return response.css("script::text").re_first(r'"priceCurrency":"(\w+)"')
+
+    def extract_product_description(self, response):
+        return [text.strip().replace("-", "")
+                for text in response.css("div.op-tab-0::text").getall()
+                if text.strip()]
+
+    def extract_product_gender(self, response):
+        return response.css("div#ariane li:nth-child(2) span[itemprop='name']::text").get()
+
+    def extract_product_image_urls(self, response):
+        return response.css(".w-100.lzy_img.d-none::attr(src)").getall()
+
+    def extract_product_language(self, response):
+        return response.css("html::attr(lang)").get().split("-")[0]
+
+    def extract_product_market(self, response):
+        return response.css("html::attr(lang)").get().split("-")[1]
+
+    def extract_product_name(self, response):
+        return response.css("h1.fs-18.mb-3.d-flex.flex-column::text")[1].getall()[0].strip()
+
+    def extract_product_price(self, response):
+        return response.css("p.text_style-11 span::text").get().split("\xa0")[0].replace(",", ".")
+
+    def extract_product_retailer_sku(self, response):
+        return response.css("link[rel='canonical']::attr(href)").get().split("-")[-1]
+
+    def extract_product_url(self, response):
+        return response.css("link[rel='canonical']::attr(href)").get()
