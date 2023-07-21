ecacebd9422978a7ce82f08e84a2a35f3eba4843
Saleha Shahzad <saleha.shahzad@192.168.10.3>
2023-07-21 12:03:20 +0500

    improved date accessing approach

--- weatherman/weather_data_parser.py
+++ weatherman/weather_data_parser.py
@@ -17,23 +17,20 @@ class WeatherDataParser:
             with open(file_path, 'r') as file:
                 reader = csv.DictReader(file)
 
-                column_names = reader.fieldnames
-                column_names[0] = 'date'
-
                 for row in reader:
-                    date = row['date']
+                    date = row.get('PKT', row.get('PKST'))
 
                     max_temp = self.clean_value(row['Max TemperatureC'])
                     min_temp = self.clean_value(row['Min TemperatureC'])
                     max_humidity = self.clean_value(row['Max Humidity'])
                     mean_humidity = self.clean_value(row[' Mean Humidity'])
 
-                    reading = WeatherReading(
+                    readings.append(WeatherReading(
                         date,
                         max_temp,
                         min_temp,
                         max_humidity,
                         mean_humidity
-                    )
-                    readings.append(reading)
+                    ))
+
         return readings
