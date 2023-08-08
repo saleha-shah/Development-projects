8cf25fbbac075cdd74b6c28d0af3bbe2d02a3745
Saleha Shahzad <saleha.shahzad@A006-01264.local>
2023-08-08 10:44:10 +0500

    merged in a file, tags removed, imports improved

--- scrapy_projects/scrapy_projects/spiders/citadium.py
+++ scrapy_projects/scrapy_projects/spiders/citadium.py
@@ -1,14 +1,14 @@
-from copy import deepcopy
-from json import loads
-from re import findall
+import copy
+import json
+import re
 
-from scrapy.spiders import CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule, CrawlSpider
 
 from scrapy_projects.items import ScrapyProjectsItem
 
 
 class ParsingSpider(CrawlSpider):
-    allowed_domains = ["www.citadium.com"]
     name = "citadium_parser"
 
     def parse_product(self, response):
@@ -25,7 +25,7 @@ class ParsingSpider(CrawlSpider):
         item["price"] = self.extract_product_price(response)
         item["retailer_sku"] = self.extract_product_retailer_sku(response)
         item["skus"] = self.extract_product_skus(response)
-        item["trail"] = deepcopy(response.meta.get("trail", []))
+        item["trail"] = copy.deepcopy(response.meta.get("trail", []))
         item["url"] = self.extract_product_url(response)
 
         yield item
@@ -34,23 +34,22 @@ class ParsingSpider(CrawlSpider):
         return response.css("p.align-items-center::text").get().strip()
 
     def extract_product_category(self, response):
-        return response.css("div.product-infos-text-seo span a::text").getall()
+        return response.css(".product-infos-text-seo a::text").getall()
 
     def extract_product_currency(self, response):
         return response.css("script::text").re_first(r'"priceCurrency":"(\w+)"')
 
     def extract_product_description(self, response):
         return [text.strip().replace("-", "")
-                for text in response.css("div.op-tab-0::text").getall()
+                for text in response.css(".op-tab-0::text").getall()
                 if text.strip()]
 
     def extract_product_gender(self, response):
-        return response.css("div#ariane li:nth-child(2) span[itemprop='name']::text").get()
+        return response.css("li:nth-child(2) [itemprop='name']::text").get()
 
     def extract_product_image_urls(self, response):
         script_content = response.css("script[type='application/ld+json']::text").get()
-        json_data = loads(script_content)
-        return json_data.get("image")
+        return json.loads(script_content).get("image")
 
     def extract_product_language(self, response):
         return response.css("html::attr(lang)").get().split("-")[0]
@@ -62,16 +61,16 @@ class ParsingSpider(CrawlSpider):
         return response.css("h1::text")[1].getall()[0].strip()
 
     def extract_product_price(self, response):
-        price = response.css("p.text_style-11 span::text").get().split("\xa0")[0].replace(",", ".")
-        return float(price)
+        price = response.css(".text_style-11 span::text").get().split("\xa0")[0].replace(",", ".")
+        return eval(price)
 
     def extract_product_retailer_sku(self, response):
         return response.css("link[rel='canonical']::attr(href)").get().split("-")[-1]
 
     def extract_product_skus(self, response):
-        script_content = findall(r'dataLayer\.push\((\{.*?\})\);',
-                                 response.xpath("//script")[1].extract())
-        json_data = loads(script_content[1])
+        script_content = re.findall(r'dataLayer\.push\((\{.*?\})\);',
+                                    response.xpath("//script")[1].extract())
+        json_data = json.loads(script_content[1])
 
         skus = {}
         product = json_data.get("ecommerce", {}).get("detail", {}).get("products", [])[0]
@@ -79,19 +78,60 @@ class ParsingSpider(CrawlSpider):
 
         for variant in variants:
             sku_id = variant.get("refId")
-            price = float(variant.get("price"))
+            price = eval(variant.get("price"))
             size = variant.get("size").replace(",", ".")
-            quantity = not variant.get("quantity")
+            out_of_stock = not variant.get("quantity")
 
             skus[sku_id] = {
-                "color": response.css("p.active-color::text").get(),
+                "color": response.css(".active-color::text").get(),
                 "currency": self.extract_product_currency(response),
                 "price": price,
                 "size": "One Size" if size == "TU" else size,
-                "out_of_stock": quantity
+                "out_of_stock": out_of_stock
                 }
 
         return skus
 
     def extract_product_url(self, response):
-        return response.css("link[rel='canonical']::attr(href)").get()
+        return response.css("[rel='canonical']::attr(href)").get()
+
+
+class CrawlingSpider(CrawlSpider):
+    name = "citadium"
+    allowed_domains = ["www.citadium.com"]
+    start_urls = ["https://www.citadium.com/fr/fr"]
+
+    listings_css = [
+        ".change-bg-anim a",
+        ".container-submenu .link_style-1",
+        "[rel='next']",
+        ".letter-header a",
+    ]
+    products_css = [
+        "#view-all-items .position-relative",
+        ".swiper-container-colors li a",
+    ]
+
+    parsing_spider = ParsingSpider()
+
+    rules = (
+        Rule(LinkExtractor(restrict_css=listings_css),
+             process_request="add_trail_and_follow"),
+        Rule(LinkExtractor(restrict_css=products_css),
+             process_request="add_trail_and_follow", callback=parsing_spider.parse_product),
+    )
+
+    def add_trail_and_follow(self, request, response):
+        request.meta.update({"trail": self.get_updated_trail(response)})
+        return request
+
+    def extract_page_name(self, response):
+        return response.css("title::text").get()
+
+    def get_updated_trail(self, response):
+        page_name = self.extract_page_name(response)
+        url = response.url
+        trail = copy.deepcopy(response.meta.get("trail", []))
+        trail.append([page_name, url])
+
+        return trail
