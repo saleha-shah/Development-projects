9143855f2ec35991df9ef33c3b7047b2d477d883
Saleha Shahzad <saleha.shahzad@192.168.10.3>
2023-08-30 10:03:18 +0500

    separate signal for ecommerce

--- django_projects/account/signals.py
+++ django_projects/account/signals.py
@@ -5,7 +5,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 from account.models import ProfileInfo
-from ecommerce.models import OrderedItem
 
 
 @receiver(post_save, sender=ProfileInfo)
@@ -20,35 +19,3 @@ def send_signup_email(sender, instance, created, **kwargs):
         })
 
         send_mail(subject, None, from_email, recipient_list, html_message=html_message)
-
-
-@receiver(post_save, sender=OrderedItem)
-def send_order_confirmation(sender, instance, created, **kwargs):
-    if created:
-        target_order_id = instance.order.order_id
-        ordered_items = OrderedItem.objects.filter(order__order_id=target_order_id)
-
-        cart_items_data = []
-        for item in ordered_items:
-            cart_items_data.append(
-                {
-                    'product_name': item.cart_item.product.name,
-                    'size': item.cart_item.size,
-                    'quantity': item.cart_item.quantity,
-                }
-            )
-            item.cart_item.delete()
-
-        subject = 'Sleek Boutique--Order Confirmation'
-        from_email = settings.EMAIL_HOST_USER
-        recipient_list = [instance.order.user.email]
-
-        context = {
-                'order': instance.order,
-                'cart_items_data': cart_items_data
-            }
-
-        html_message = render_to_string('email_templates/order_confirmation.html', context)
-        message = 'Thank you for placing an order with us!'
-
-        send_mail(subject, message, from_email, recipient_list, html_message=html_message)
---
+++ django_projects/ecommerce/signals.py
@@ -0,0 +1,39 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+from ecommerce.models import OrderedItem
+
+
+@receiver(post_save, sender=OrderedItem)
+def send_order_confirmation(sender, instance, created, **kwargs):
+    if created:
+        target_order_id = instance.order.order_id
+        ordered_items = OrderedItem.objects.filter(order__order_id=target_order_id)
+
+        cart_items_data = []
+        for item in ordered_items:
+            cart_items_data.append(
+                {
+                    'product_name': item.cart_item.product.name,
+                    'size': item.cart_item.size,
+                    'quantity': item.cart_item.quantity,
+                }
+            )
+            item.cart_item.delete()
+
+        subject = 'Sleek Boutique--Order Confirmation'
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [instance.order.user.email]
+
+        context = {
+                'order': instance.order,
+                'cart_items_data': cart_items_data
+            }
+
+        html_message = render_to_string('email_templates/order_confirmation.html', context)
+        message = 'Thank you for placing an order with us!'
+
+        send_mail(subject, message, from_email, recipient_list, html_message=html_message)
